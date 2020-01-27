7cc2b608c788518e653845846f58727a3d231736
Antonio Lain <antlai@cafjs.com>
2020-01-26 23:18:52 -0800

    Replace var by const

--- test/test-forward.js
+++ test/test-forward.js
@@ -1,18 +1,18 @@
-"use strict"
+'use strict';
 
-var hello = require('./hello/main.js');
-var app = hello;
-var caf_core = require('caf_core');
-var myUtils = caf_core.caf_components.myUtils;
-var async = caf_core.async;
-var cli = caf_core.caf_cli;
-var request = require('request');
+const hello = require('./hello/main.js');
+const app = hello;
+const caf_core = require('caf_core');
+const myUtils = caf_core.caf_components.myUtils;
+const async = caf_core.async;
+const cli = caf_core.caf_cli;
+const request = require('request');
 
-var CA_OWNER_1='forwardother1';
-var CA_LOCAL_NAME_1='admin';
-var CA_LOCAL_NAME_2='foo';
-var FROM_1 =  CA_OWNER_1 + '-' + CA_LOCAL_NAME_1;
-var FROM_2 =  CA_OWNER_1 + '-' + CA_LOCAL_NAME_2;
+const CA_OWNER_1='forwardother1';
+const CA_LOCAL_NAME_1='admin';
+const CA_LOCAL_NAME_2='foo';
+const FROM_1 =  CA_OWNER_1 + '-' + CA_LOCAL_NAME_1;
+const FROM_2 =  CA_OWNER_1 + '-' + CA_LOCAL_NAME_2;
 
 
 process.on('uncaughtException', function (err) {
@@ -22,7 +22,7 @@ process.on('uncaughtException', function (err) {
 
 });
 
-var getIt = function(test, path, num, cb) {
+const getIt = function(test, path, num, cb) {
     request.get('http://127.0.0.1:3000/' + path, {},
                 function (error, response, body) {
                     test.ifError(error);
@@ -37,7 +37,7 @@ var getIt = function(test, path, num, cb) {
 
 module.exports = {
     setUp: function (cb) {
-       var self = this;
+       const self = this;
         app.init( {name: 'top'}, 'framework.json', null,
                       function(err, $) {
                           if (err) {
@@ -52,7 +52,7 @@ module.exports = {
                       });
     },
     tearDown: function (cb) {
-        var self = this;
+        const self = this;
         if (!this.$) {
             cb(null);
         } else {
@@ -63,8 +63,8 @@ module.exports = {
     forward: function (test) {
         test.expect(10);
         var s1;
-        var from1 = FROM_1;
-        var from2 = FROM_2;
+        const from1 = FROM_1;
+        const from2 = FROM_2;
         async.series([
             function(cb) {
                 s1 = new cli.Session('ws://foo-xx.vcap.me:3000', from1, {
@@ -75,18 +75,19 @@ module.exports = {
                 };
             },
             function(cb) {
-                var cb1 = function(err) {
+                const cb1 = function(err) {
                     test.ifError(err);
                     cb(null);
                 };
-                s1.setBinding(from2, 'http://foo-xx.vcap.me:3000/forwardother1-bar1',
-                              cb1);
+                s1.setBinding(
+                    from2, 'http://foo-xx.vcap.me:3000/forwardother1-bar1', cb1
+                );
             },
             function(cb) {
                 getIt(test, from2 + '/three', 3, cb);
             },
             function(cb) {
-                var cb1 = function(err) {
+                const cb1 = function(err) {
                     test.ifError(err);
                     cb(null);
                 };
